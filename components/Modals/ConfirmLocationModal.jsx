01afc9fa728ba6c669d0896e9656b169c7f477a2
Treasurechic <babychic140@gmail.com>
2022-07-29 18:36:13 +0100

    :sparkles: Restrict USA residents

--- components/Modals/ConfirmLocationModal.jsx
+++ components/Modals/ConfirmLocationModal.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import Image from 'next/image'
 
 // Material UI components
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import Modal from '@mui/material/Modal'
 import Button from '@mui/material/Button'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import TelegramIcon from '@mui/icons-material/Telegram'
+import RedditIcon from '@mui/icons-material/Reddit'
 
 // Custom components
 import { ConnectWalletPrompt } from './ConnectWalletPrompt'
@@ -13,10 +17,14 @@ import { SelectCountry } from './SelectCountry'
 
 export const ConfirmLocationModal = ({ open, handleClose, connectWallet }) => {
   const [openSuccessModal, setOpenSuccessModal] = useState(false)
+  const [openFailedModal, setOpenFailedModal] = useState(false)
   const [countryValue, setCountryValue] = useState({})
 
   const toggleSuccessModal = () => {
     setOpenSuccessModal(!openSuccessModal)
+  }
+  const toggleFailedModal = () => {
+    setOpenFailedModal(!openFailedModal)
   }
 
   const clearandClose = () => {
@@ -25,9 +33,12 @@ export const ConfirmLocationModal = ({ open, handleClose, connectWallet }) => {
   }
 
   const handleConfirm = () => {
-    if (countryValue.code) {
+    if (countryValue.code && countryValue.code != 'US') {
       clearandClose()
       toggleSuccessModal()
+    } else {
+      clearandClose()
+      toggleFailedModal()
     }
   }
 
@@ -48,7 +59,7 @@ export const ConfirmLocationModal = ({ open, handleClose, connectWallet }) => {
         <Box
           sx={{
             backgroundColor: 'secondary.main',
-            color: 'secondary.contrastText',
+            color: 'primary.contrastText',
             borderRadius: '3px',
             padding: '1rem',
             marginBlock: '1.2rem',
@@ -100,7 +111,7 @@ export const ConfirmLocationModal = ({ open, handleClose, connectWallet }) => {
         <Box
           sx={{
             backgroundColor: 'secondary.main',
-            color: 'secondary.contrastText',
+            color: 'primary.contrastText',
             borderRadius: '3px',
             padding: '1rem',
             marginBlock: '1.2rem',
@@ -135,6 +146,131 @@ export const ConfirmLocationModal = ({ open, handleClose, connectWallet }) => {
             }}
             countryValue={countryValue}
           />
+        </Box>
+      </Modal>
+      <Modal
+        open={openFailedModal}
+        onClose={toggleFailedModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{
+          width: '380px',
+          maxWidth: '93%',
+          marginTop: '20vh',
+          marginInline: 'auto',
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: 'secondary.main',
+            color: 'primary.contrastText',
+            borderRadius: '3px',
+            padding: '1rem',
+            marginBlock: '1.2rem',
+            border: '1px solid',
+            borderColor: 'secondary.light2',
+          }}
+        >
+          <Typography
+            variant="p"
+            fontWeight={700}
+            fontSize="1.1rem"
+            marginBottom={'1rem'}
+          >
+            Your country is not supported yet
+          </Typography>
+          <Typography
+            variant="p"
+            fontWeight={500}
+            fontSize="1rem"
+            marginBottom={'1.5rem'}
+          >
+            At this time, your country is not eligible to sign up for rewards.
+            Your country may become eligible in the future. Keep an eye on our
+            social channels for updates.
+          </Typography>
+          <hr />
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              columnGap: '0.8rem',
+              marginLeft: '1rem',
+              marginBottom: '1rem',
+            }}
+          >
+            <TwitterIcon sx={{ fontSize: '1.8rem' }} />
+            <a
+              style={{ color: 'inherit', fontWeight: 700 }}
+              href="https://twitter.com/AlgodexOfficial"
+              target="_blank"
+              rel="noreferrer"
+            >
+              @AlgodexOfficial
+            </a>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              columnGap: '0.8rem',
+              marginLeft: '1rem',
+              marginBottom: '1rem',
+            }}
+          >
+            <TelegramIcon sx={{ fontSize: '2.1rem' }} />
+            <a
+              style={{ color: 'inherit', fontWeight: 700 }}
+              href="http://t.me/algodex"
+              target="_blank"
+              rel="noreferrer"
+            >
+              t.me/algodex
+            </a>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              columnGap: '0.8rem',
+              marginLeft: '1rem',
+              marginBottom: '1rem',
+            }}
+          >
+            <Image
+              src="/discord.png"
+              alt="Discord Logo"
+              width="24"
+              height="24"
+            />
+            <a
+              style={{ color: 'inherit', fontWeight: 700 }}
+              href="https://discord.gg/qS3Q7AqwF6"
+              target="_blank"
+              rel="noreferrer"
+            >
+              discord.gg/qS3Q7AqwF6
+            </a>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              columnGap: '0.8rem',
+              marginLeft: '1rem',
+              marginBottom: '1rem',
+            }}
+          >
+            <RedditIcon sx={{ fontSize: '1.8rem' }} />
+            <a
+              style={{ color: 'inherit', fontWeight: 700 }}
+              href="https://www.reddit.com/r/Algodex/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              r/Algodex
+            </a>
+          </Box>
         </Box>
       </Modal>
     </>
