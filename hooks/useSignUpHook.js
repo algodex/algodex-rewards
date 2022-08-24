9685cf71d10c210635140b7c3021d0949fde12e4
Treasurechic <babychic140@gmail.com>
2022-08-24 21:02:01 +0100

    :rotating_light: Fixed lint errors

--- hooks/useSignUpHook.js
+++ hooks/useSignUpHook.js
@@ -1,27 +1,27 @@
-import { signUpForRewards } from "@/lib/send_transaction";
-import { useContext, useState } from "react";
-import { WalletsContext } from "./useWallets";
+import { signUpForRewards } from '@/lib/send_transaction'
+import { useContext, useState } from 'react'
+import { WalletsContext } from './useWallets'
 
 export const useSignUpHook = ({ setWalletSignedUp, activeWallet }) => {
-  const { walletConnect } = useContext(WalletsContext);
+  const { walletConnect } = useContext(WalletsContext)
   const [actionStatus, setActionStatus] = useState({
-    message: "",
+    message: '',
     success: false,
-  });
-  const [loading, setLoading] = useState(false);
-  const [openModal, setOpenModal] = useState(false);
+  })
+  const [loading, setLoading] = useState(false)
+  const [openModal, setOpenModal] = useState(false)
 
   const signUp = async () => {
-    setLoading(true);
+    setLoading(true)
     const response = await signUpForRewards({
       ...activeWallet,
       connector:
         walletConnect.current?.accounts[0] == activeWallet.address
           ? walletConnect.current
           : null,
-    });
-    setLoading(false);
-    console.debug(response);
+    })
+    setLoading(false)
+    console.debug(response)
     if (response instanceof Error) {
       if (
         /Transaction Request Rejected: The User has rejected the transaction request/.test(
@@ -30,9 +30,9 @@ export const useSignUpHook = ({ setWalletSignedUp, activeWallet }) => {
       ) {
         setActionStatus({
           message:
-            "You rejected the 0.00 transaction request required to sign you up for rewards",
+            'You rejected the 0.00 transaction request required to sign you up for rewards',
           success: false,
-        });
+        })
       } else if (
         /disconnected/.test(response) ||
         /Cannot read properties of null/.test(response)
@@ -40,46 +40,46 @@ export const useSignUpHook = ({ setWalletSignedUp, activeWallet }) => {
         setActionStatus({
           message:
             // eslint-disable-next-line max-len
-            "This wallet is disconnected, kindly reconnect wallet and try again",
+            'This wallet is disconnected, kindly reconnect wallet and try again',
           success: false,
-        });
+        })
       } else if (/Network mismatch between dApp and Wallet/.test(response)) {
         setActionStatus({
           message:
             // eslint-disable-next-line max-len
-            "Network mismatch, wallet is connected to TestNet and dApp connected to MainNet (or vice versa)",
+            'Network mismatch, wallet is connected to TestNet and dApp connected to MainNet (or vice versa)',
           success: false,
-        });
+        })
       } else if (/overspend/.test(response)) {
         setActionStatus({
           message:
-            "This transaction request failed, requires a 0.01 Algo network fee.",
+            'This transaction request failed, requires a 0.01 Algo network fee.',
           success: false,
-        });
+        })
       } else if (
         /PopupOpenError|blocked|Can not open popup window/.test(response)
       ) {
         setActionStatus({
           message:
-            "Please disable your popup blocker (likely in the top-right of your browser window)",
+            'Please disable your popup blocker (likely in the top-right of your browser window)',
           success: false,
-        });
+        })
       } else {
         setActionStatus({
-          message: "Sorry, an error occurred",
+          message: 'Sorry, an error occurred',
           success: false,
-        });
+        })
       }
     } else {
-      setWalletSignedUp(true);
+      setWalletSignedUp(true)
       setActionStatus({
         message:
           // eslint-disable-next-line max-len
-          "This wallet was successfully connected and is signed up for rewards! Continue to view your rewards in the app.",
+          'This wallet was successfully connected and is signed up for rewards! Continue to view your rewards in the app.',
         success: true,
-      });
+      })
     }
-    setOpenModal(true);
-  };
-  return { loading, openModal, setOpenModal, actionStatus, signUp };
-};
+    setOpenModal(true)
+  }
+  return { loading, openModal, setOpenModal, actionStatus, signUp }
+}
--- hooks/useWalletConnect.js
+++ hooks/useWalletConnect.js
@@ -1,11 +1,11 @@
-import { useCallback, useContext, useEffect, useRef } from "react";
+import { useCallback, useContext, useEffect, useRef } from 'react'
 
-import QRCodeModal from "algorand-walletconnect-qrcode-modal";
-import { WalletsContext } from "./useWallets";
+import QRCodeModal from 'algorand-walletconnect-qrcode-modal'
+import { WalletsContext } from './useWallets'
 const ERROR = {
-  FAILED_TO_INIT: "WalletConnect Wallet failed to initialize.",
-  FAILED_TO_CONNECT: "WalletConnect Wallet failed to connect.",
-};
+  FAILED_TO_INIT: 'WalletConnect Wallet failed to initialize.',
+  FAILED_TO_CONNECT: 'WalletConnect Wallet failed to connect.',
+}
 
 /**
  * Use Wallet Connect query
@@ -18,83 +18,82 @@ export default function useWalletConnect(onConnect, onDisconnect) {
    * Instance referenc
    */
   // const walletConnect = useRef()
-  const { walletConnect } = useContext(WalletsContext);
+  const { walletConnect } = useContext(WalletsContext)
   const connect = async () => {
     try {
       // Something went wrong!
       if (!walletConnect.current) {
-        console.error(ERROR.FAILED_TO_INIT);
-        return;
+        console.error(ERROR.FAILED_TO_INIT)
+        return
       }
       // Check if connection is already established
       if (!walletConnect.current.connected) {
-        console.log("Creating Session");
+        console.log('Creating Session')
         // create new session
-        walletConnect.current.createSession();
+        walletConnect.current.createSession()
       } else {
-        walletConnect.current.killSession();
+        walletConnect.current.killSession()
         setTimeout(() => {
-          walletConnect.current.createSession();
-        }, 1000);
+          walletConnect.current.createSession()
+        }, 1000)
       }
-
     } catch (e) {
-      console.error(ERROR.FAILED_TO_CONNECT, e);
+      console.error(ERROR.FAILED_TO_CONNECT, e)
     }
-  };
+  }
   const disconnect = () => {
     if (walletConnect.current.connected) {
-      onDisconnect(walletConnect.current.accounts[0]);
-      walletConnect.current.killSession();
+      onDisconnect(walletConnect.current.accounts[0])
+      walletConnect.current.killSession()
     }
-  };
+  }
 
   const handleDisconnect = useCallback(
     (err) => {
-      console.log("DISCONNECTED");
-      if (err) throw err;
-      localStorage.removeItem("walletconnect");
-      onDisconnect(walletConnect.current.accounts[0]);
+      console.log('DISCONNECTED')
+      if (err) throw err
+      localStorage.removeItem('walletconnect')
+      onDisconnect(walletConnect.current.accounts[0])
     },
     [onDisconnect]
-  );
+  )
 
   const handleConnected = (err, payload) => {
-    console.log("CONNECTED");
+    console.log('CONNECTED')
     if (err) {
-      throw err;
+      throw err
     }
 
-    let accounts = [];
+    let accounts = []
 
     // Get provided accounts
-    if (typeof payload !== "undefined" && Array.isArray(payload.params)) {
-      accounts = payload.params[0].accounts;
+    if (typeof payload !== 'undefined' && Array.isArray(payload.params)) {
+      accounts = payload.params[0].accounts
     }
 
     // Map the connector to the address list
     const _addresses = accounts.map((acct) => ({
-      type: "wallet-connect",
+      type: 'wallet-connect',
       connector: walletConnect.current,
       address: acct,
-    }));
-    onConnect(_addresses);
-    QRCodeModal.close();
-  };
+    }))
+    onConnect(_addresses)
+    QRCodeModal.close()
+  }
   useEffect(() => {
     // let listener;
-    if (typeof walletConnect.current !== "undefined") {
-      walletConnect.current.on("connect", handleConnected);
-      walletConnect.current.on("session_update", handleConnected);
-      walletConnect.current.on("disconnect", handleDisconnect);
+    if (typeof walletConnect.current !== 'undefined') {
+      walletConnect.current.on('connect', handleConnected)
+      walletConnect.current.on('session_update', handleConnected)
+      walletConnect.current.on('disconnect', handleDisconnect)
     }
     return () => {
-      if (typeof walletConnect.current !== "undefined") {
-        walletConnect.current.off("connect");
-        walletConnect.current.off("session_update");
-        walletConnect.current.off("disconnect");
+      if (typeof walletConnect.current !== 'undefined') {
+        walletConnect.current.off('connect')
+        walletConnect.current.off('session_update')
+        walletConnect.current.off('disconnect')
       }
-    };
-  }, [walletConnect.current]);
-  return { connect, disconnect, connector: walletConnect.current };
+    }
+  }, [walletConnect.current])
+  return { connect, disconnect, connector: walletConnect.current }
 }
