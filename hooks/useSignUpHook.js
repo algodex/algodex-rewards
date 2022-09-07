9d9704230f8c06f503b903f4ff6aeaaa76a1547d
Treasurechic <babychic140@gmail.com>
2022-09-07 23:06:27 +0100

    :sparkles: Hide optin button if wallet has opted in

--- hooks/useSignUpHook.js
+++ hooks/useSignUpHook.js
@@ -25,7 +25,6 @@ export const useSignUpHook = ({ setWalletSignedUp, activeWallet }) => {
   }
 
   useEffect(() => {
-    console.log('first')
     if (activeWallet) {
       checkStatus(activeWallet)
     }
