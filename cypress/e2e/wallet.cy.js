38da3981e58dc338bc6fa883b2bcb1970b20a52d
Treasurechic <babychic140@gmail.com>
2022-09-19 12:11:43 +0100

    :green_heart: Add 4 secs wait in wallet test

--- cypress/e2e/wallet.cy.js
+++ cypress/e2e/wallet.cy.js
@@ -12,6 +12,8 @@ describe('Algodex Rewards Wallet page', () => {
     cy.fixture('wallet').then(function (walletData) {
       cy.visit(`/wallet?viewAsWallet=${walletData.address}`)
     })
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(4000)
     cy.get('[data-testid=address]').click()
     cy.get('[data-testid=balance]').should('be.visible')
     cy.get('[data-testid=disconnect]').should('be.visible')
