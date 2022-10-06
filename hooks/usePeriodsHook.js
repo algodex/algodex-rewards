5788a56e0e7078ffb77c7ead8278f89be34e5f5a
Treasurechic <babychic140@gmail.com>
2022-10-06 16:44:32 +0100

    :sparkles: Display local UTC

--- hooks/usePeriodsHook.js
+++ hooks/usePeriodsHook.js
@@ -32,6 +32,9 @@ export const usePeriodsHook = ({ activeWallet, isMobile }) => {
   const pendingPeriod = useMemo(() => {
     const curr_unix = Math.floor(new Date().getTime() / 1000)
     const epoch = Math.floor((curr_unix - getEpochStart(1)) / 604800 + 1)
+    const UTC = DateTime.fromJSDate(
+      new Date(getEpochStart(epoch) * 1000)
+    ).offsetNameShort
     const start = DateTime.fromJSDate(
       new Date(getEpochStart(epoch) * 1000)
     ).toLocaleString(DateTime.DATETIME_MED)
@@ -41,7 +44,7 @@ export const usePeriodsHook = ({ activeWallet, isMobile }) => {
     ).toLocaleString(DateTime.DATETIME_MED)
 
     return {
-      date: `${start} - ${end}`,
+      date: `${start} - ${end} ${UTC}`,
       number: epoch,
     }
   }, [])
